edc91fe838510e42b7511353c5f366ceee7ed170
GrinOnFace <109618694+GrinOnFace@users.noreply.github.com>
2024-05-10 22:14:16 +0300

    microfrontend/10.05

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.scss"
+import './App.scss'
+import { Outlet, Link } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div className="hello">
-      hello world
+    <div className="">
+      <Link to={'/about'}>about</Link>
+      <Link to={'/shop'}>shop</Link>
+      <h1 className="value">wgege</h1>
+      <Outlet/>
     </div>
   )
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,62 +1,25 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from 'path';
 import webpack from 'webpack';
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-
-type Mode = "production" | "development"
+import { buildWebpack } from './config/build/buildWebpack';
+import { buildMode, buildPaths } from './config/build/types/types';
 
 interface EnvVar {
-  mode: Mode,
+  mode: buildMode,
   port: number,
 }
 
 export default (env: EnvVar) => {
-
-  const isDev = env.mode === "development";
-  const isProd = env.mode === "production";
-
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
+  const paths: buildPaths = {
+    output: path.resolve(__dirname, 'build'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: '[name].[contenthash].js',
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-      isProd && new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    devtool: isDev ? 'inline-source-map' : false,
-    devServer: isDev ? {
-      port: env.port ?? 3000,
-      open: true,
-    } : undefined,
+    html: path.resolve(__dirname, 'public', 'index.html')
   }
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? 'development',
+    paths: paths
+  });
 
   return config
 };
